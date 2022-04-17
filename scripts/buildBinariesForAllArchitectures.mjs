02213b6ef7601eacea0b278a3a02e5a151b9a37d
Grunet <sadikshahidain@gmail.com>
2022-04-17 02:40:06 +0000

    Fix formatting

--- scripts/buildBinariesForAllArchitectures.mjs
+++ scripts/buildBinariesForAllArchitectures.mjs
@@ -6,5 +6,5 @@ const targetArchitectures = getTargetArchitectures();
 
 for (const architecture of targetArchitectures) {
   await $
-    `make buildBinaryForASingleArchitecture architecture=${architecture} outputPath=./release_artifacts/${architecture}/cfn-trace`
+    `make buildBinaryForASingleArchitecture architecture=${architecture} outputPath=./release_artifacts/${architecture}/cfn-trace`;
 }
