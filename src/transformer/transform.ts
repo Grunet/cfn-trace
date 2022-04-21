f2f15f0ba802d8f3e36ef4927269abe6456e2cfa
Grunet <sadikshahidain@gmail.com>
2022-04-21 03:37:27 +0000

    Finish fleshing out the output data structure

--- src/transformer/transform.ts
+++ src/transformer/transform.ts
@@ -10,10 +10,26 @@ interface IDependencies {
 }
 
 interface ITracingData {
+  spanDataById: Map<string, ISpanData>; //TODO - is "resourceIdPerCLoudformation" the best thing to go by here?
+}
+
+interface ISpanData {
+  childSpanIds: Set<string>;
+  name: string;
+  startInstant: Date;
+  endInstant: Date;
 }
 
 function transformStackEventDataIntoTracingData(inputs: IInputs): ITracingData {
   const { stackName, dependencies: { cloudformationClientAdapter } } = inputs;
+
+  const tracingData = new Map<string, ISpanData>();
+
+  //TODO - fill in the details
+
+  return {
+    spanDataById: tracingData,
+  };
 }
 
 export { transformStackEventDataIntoTracingData };
