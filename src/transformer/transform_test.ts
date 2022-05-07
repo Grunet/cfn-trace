eb51ee2511b29df32df89e8b46e0c98892f73b5e
Grunet <sadikshahidain@gmail.com>
2022-05-07 05:00:40 +0000

    Update test to better reflect how creation of a nested stack looks like

--- src/transformer/transform_test.ts
+++ src/transformer/transform_test.ts
@@ -8,7 +8,166 @@ import { IAdaptedStackEvent } from "../cloudformationClientAdapter/client.ts";
 
 import { assertEquals } from "https://deno.land/std@0.132.0/testing/asserts.ts";
 
-Deno.test("Correctly transforms the events from a 2-tier nested stack with 1 non-stack resource in each", async () => {
+Deno.test("Correctly transforms the events from creating a 2-tier nested stack with 1 non-stack resource in each", async () => {
+  //ARRANGE
+  const inputs: IInputs = {
+    stackName: "rootStackName",
+    dependencies: {
+      cloudformationClientAdapter: {
+        getEventsFromMostRecentDeploy({ stackName }) {
+          switch (stackName) {
+            case "rootStackName":
+              return Promise.resolve({
+                stackEvents: [
+                  createStackEvent({
+                    resourceIdPerCloudformation: "rootStackName",
+                    resourceIdPerTheServiceItsFrom:
+                      "arn:aws:cloudformation:us-east-1:000000000000:stack/rootStackName/00aa00a0-a00a-00aa-0a00-00a0a0a00000",
+                    resourceStatus: "CREATE_COMPLETE",
+                    resourceType: "AWS::CloudFormation::Stack",
+                    timestamp: new Date("2022-04-11T00:00:30.000Z"),
+                  }),
+                  createStackEvent({
+                    resourceIdPerCloudformation: "FirstNestedStackResourceName",
+                    resourceIdPerTheServiceItsFrom:
+                      "arn:aws:cloudformation:us-east-1:000000000000:stack/rootStackName-FirstNestedStackResourceName-AAAA0AAAAAA/00aa00a0-a00a-00aa-0a00-00a0a0a00000",
+                    resourceStatus: "CREATE_COMPLETE",
+                    resourceType: "AWS::CloudFormation::Stack",
+                    timestamp: new Date("2022-04-11T00:00:20.000Z"),
+                  }),
+                  createStackEvent({
+                    resourceIdPerCloudformation: "FirstNestedStackResourceName",
+                    resourceIdPerTheServiceItsFrom: "somePlaceholder",
+                    resourceStatus: "CREATE_IN_PROGRESS",
+                    resourceType: "AWS::CloudFormation::Stack",
+                    timestamp: new Date("2022-04-11T00:00:15.000Z"),
+                  }),
+                  createStackEvent({
+                    resourceIdPerCloudformation: "TheEcsCluster",
+                    resourceIdPerTheServiceItsFrom: "TheClusterName",
+                    resourceStatus: "CREATE_COMPLETE",
+                    resourceType: "AWS::ECS::Cluster",
+                    timestamp: new Date("2022-04-11T00:00:10.000Z"),
+                  }),
+                  createStackEvent({
+                    resourceIdPerCloudformation: "TheEcsCluster",
+                    resourceIdPerTheServiceItsFrom: "somePlaceholder",
+                    resourceStatus: "CREATE_IN_PROGRESS",
+                    resourceType: "AWS::ECS::Cluster",
+                    timestamp: new Date("2022-04-11T00:00:05.000Z"),
+                  }),
+                  createStackEvent({
+                    resourceIdPerCloudformation: "rootStackName",
+                    resourceIdPerTheServiceItsFrom: "somePlaceholder",
+                    resourceStatus: "CREATE_IN_PROGRESS",
+                    resourceType: "AWS::CloudFormation::Stack",
+                    timestamp: new Date("2022-04-11T00:00:00.000Z"),
+                  }),
+                ],
+              });
+            case "rootStackName-FirstNestedStackResourceName-AAAA0AAAAAA":
+              return Promise.resolve({
+                stackEvents: [
+                  createStackEvent({
+                    resourceIdPerCloudformation:
+                      "rootStackName-FirstNestedStackResourceName-AAAA0AAAAAA",
+                    resourceIdPerTheServiceItsFrom:
+                      "arn:aws:cloudformation:us-east-1:000000000000:stack/rootStackName-FirstNestedStackResourceName-AAAA0AAAAAA/00aa00a0-a00a-00aa-0a00-00a0a0a00000",
+                    resourceStatus: "CREATE_COMPLETE",
+                    resourceType: "AWS::CloudFormation::Stack",
+                    timestamp: new Date("2022-04-11T00:00:19.000Z"),
+                  }),
+                  createStackEvent({
+                    resourceIdPerCloudformation: "TheEcsService",
+                    resourceIdPerTheServiceItsFrom: "TheEcsServiceName",
+                    resourceStatus: "CREATE_COMPLETE",
+                    resourceType: "AWS::ECS::Service",
+                    timestamp: new Date("2022-04-11T00:00:18.000Z"),
+                  }),
+                  createStackEvent({
+                    resourceIdPerCloudformation: "TheEcsService",
+                    resourceIdPerTheServiceItsFrom: "somePlaceholder",
+                    resourceStatus: "CREATE_IN_PROGRESS",
+                    resourceType: "AWS::ECS::Service",
+                    timestamp: new Date("2022-04-11T00:00:17.000Z"),
+                  }),
+                  createStackEvent({
+                    resourceIdPerCloudformation:
+                      "rootStackName-FirstNestedStackResourceName-AAAA0AAAAAA",
+                    resourceIdPerTheServiceItsFrom: "somePlaceholder",
+                    resourceStatus: "CREATE_IN_PROGRESS",
+                    resourceType: "AWS::CloudFormation::Stack",
+                    timestamp: new Date("2022-04-11T00:00:16.000Z"),
+                  }),
+                ],
+              });
+            default:
+              throw new Error(`${stackName} not found in the mock's setup`);
+          }
+        },
+      },
+    },
+  };
+
+  //ACT
+  const outputs = await transformStackEventDataIntoTracingData(inputs);
+
+  //ASSERT
+  const spanDataByConstructedId = new Map<string, ISpanData>();
+  //From root stack
+  const rootConstructedId =
+    "rootStackName-arn:aws:cloudformation:us-east-1:000000000000:stack/rootStackName/00aa00a0-a00a-00aa-0a00-00a0a0a00000-AWS::CloudFormation::Stack";
+
+  spanDataByConstructedId.set(
+    "rootStackName-arn:aws:cloudformation:us-east-1:000000000000:stack/rootStackName/00aa00a0-a00a-00aa-0a00-00a0a0a00000-AWS::CloudFormation::Stack",
+    {
+      childSpanIds: new Set<string>([
+        "TheEcsCluster-TheClusterName-AWS::ECS::Cluster",
+        "FirstNestedStackResourceName-arn:aws:cloudformation:us-east-1:000000000000:stack/rootStackName-FirstNestedStackResourceName-AAAA0AAAAAA/00aa00a0-a00a-00aa-0a00-00a0a0a00000-AWS::CloudFormation::Stack",
+      ]),
+      name: "rootStackName",
+      startInstant: new Date("2022-04-11T00:00:00.000Z"),
+      endInstant: new Date("2022-04-11T00:00:30.000Z"),
+    },
+  );
+  spanDataByConstructedId.set(
+    "TheEcsCluster-TheClusterName-AWS::ECS::Cluster",
+    {
+      childSpanIds: new Set<string>(),
+      name: "TheEcsCluster",
+      startInstant: new Date("2022-04-11T00:00:05.000Z"),
+      endInstant: new Date("2022-04-11T00:00:10.000Z"),
+    },
+  );
+  //1st nested stack as resource of the root stack
+  spanDataByConstructedId.set(
+    "FirstNestedStackResourceName-arn:aws:cloudformation:us-east-1:000000000000:stack/rootStackName-FirstNestedStackResourceName-AAAA0AAAAAA/00aa00a0-a00a-00aa-0a00-00a0a0a00000-AWS::CloudFormation::Stack",
+    {
+      childSpanIds: new Set<string>([
+        "TheEcsService--AWS::ECS::Service",
+      ]),
+      name: "FirstNestedStackResourceName",
+      startInstant: new Date("2022-04-11T00:00:15.000Z"),
+      endInstant: new Date("2022-04-11T00:00:20.000Z"),
+    },
+  );
+  //From 1st nested stack's resources
+  spanDataByConstructedId.set("TheEcsService--AWS::ECS::Service", {
+    childSpanIds: new Set<string>(),
+    name: "TheEcsService",
+    startInstant: new Date("2022-04-11T00:00:17.000Z"),
+    endInstant: new Date("2022-04-11T00:00:18.000Z"),
+  });
+
+  const expectedOutputs: ITracingData = {
+    spanDataByConstructedId,
+    rootConstructedId,
+  };
+
+  assertEquals(outputs, expectedOutputs);
+});
+
+Deno.test("Correctly transforms the events from updating a 2-tier nested stack with 1 non-stack resource in each", async () => {
   //ARRANGE
   const inputs: IInputs = {
     stackName: "rootStackName",
@@ -81,14 +240,14 @@ Deno.test("Correctly transforms the events from a 2-tier nested stack with 1 non
                   }),
                   createStackEvent({
                     resourceIdPerCloudformation: "TheEcsService",
-                    resourceIdPerTheServiceItsFrom: "", //Simulates the resource having been newly created during this deploy
+                    resourceIdPerTheServiceItsFrom: "TheEcsServiceName",
                     resourceStatus: "UPDATE_COMPLETE",
                     resourceType: "AWS::ECS::Service",
                     timestamp: new Date("2022-04-11T00:00:18.000Z"),
                   }),
                   createStackEvent({
                     resourceIdPerCloudformation: "TheEcsService",
-                    resourceIdPerTheServiceItsFrom: "", //Simulates the resource having been newly created during this deploy
+                    resourceIdPerTheServiceItsFrom: "TheEcsServiceName",
                     resourceStatus: "UPDATE_IN_PROGRESS",
                     resourceType: "AWS::ECS::Service",
                     timestamp: new Date("2022-04-11T00:00:17.000Z"),
@@ -147,7 +306,7 @@ Deno.test("Correctly transforms the events from a 2-tier nested stack with 1 non
     "FirstNestedStackResourceName-arn:aws:cloudformation:us-east-1:000000000000:stack/rootStackName-FirstNestedStackResourceName-AAAA0AAAAAA/00aa00a0-a00a-00aa-0a00-00a0a0a00000-AWS::CloudFormation::Stack",
     {
       childSpanIds: new Set<string>([
-        "TheEcsService--AWS::ECS::Service",
+        "TheEcsService-TheEcsServiceName-AWS::ECS::Service",
       ]),
       name: "FirstNestedStackResourceName",
       startInstant: new Date("2022-04-11T00:00:15.000Z"),
@@ -155,106 +314,13 @@ Deno.test("Correctly transforms the events from a 2-tier nested stack with 1 non
     },
   );
   //From 1st nested stack's resources
-  spanDataByConstructedId.set("TheEcsService--AWS::ECS::Service", {
-    childSpanIds: new Set<string>(),
-    name: "TheEcsService",
-    startInstant: new Date("2022-04-11T00:00:17.000Z"),
-    endInstant: new Date("2022-04-11T00:00:18.000Z"),
-  });
-
-  const expectedOutputs: ITracingData = {
-    spanDataByConstructedId,
-    rootConstructedId,
-  };
-
-  assertEquals(outputs, expectedOutputs);
-});
-
-Deno.test("Correctly transforms the events from a freshly created stack", async () => {
-  //ARRANGE
-  const inputs: IInputs = {
-    stackName: "rootStackName",
-    dependencies: {
-      cloudformationClientAdapter: {
-        getEventsFromMostRecentDeploy({ stackName }) {
-          switch (stackName) {
-            case "rootStackName":
-              return Promise.resolve({
-                stackEvents: [
-                  createStackEvent({
-                    resourceIdPerCloudformation: "rootStackName",
-                    resourceIdPerTheServiceItsFrom:
-                      "arn:aws:cloudformation:us-east-1:000000000000:stack/rootStackName/00aa00a0-a00a-00aa-0a00-00a0a0a00000",
-                    resourceStatus: "CREATE_COMPLETE",
-                    resourceType: "AWS::CloudFormation::Stack",
-                    timestamp: new Date("2022-04-11T00:00:30.000Z"),
-                  }),
-                  createStackEvent({
-                    resourceIdPerCloudformation: "TheEcsCluster",
-                    resourceIdPerTheServiceItsFrom: "TheClusterName",
-                    resourceStatus: "CREATE_COMPLETE",
-                    resourceType: "AWS::ECS::Cluster",
-                    timestamp: new Date("2022-04-11T00:00:15.000Z"),
-                  }),
-                  createStackEvent({
-                    resourceIdPerCloudformation: "TheEcsCluster",
-                    resourceIdPerTheServiceItsFrom: "TheClusterName",
-                    resourceStatus: "CREATE_IN_PROGRESS",
-                    resourceType: "AWS::ECS::Cluster",
-                    timestamp: new Date("2022-04-11T00:00:10.000Z"),
-                  }),
-                  createStackEvent({
-                    resourceIdPerCloudformation: "rootStackName",
-                    resourceIdPerTheServiceItsFrom:
-                      "arn:aws:cloudformation:us-east-1:000000000000:stack/rootStackName/00aa00a0-a00a-00aa-0a00-00a0a0a00000",
-                    resourceStatus: "CREATE_IN_PROGRESS",
-                    resourceType: "AWS::CloudFormation::Stack",
-                    timestamp: new Date("2022-04-11T00:00:05.000Z"),
-                  }),
-                  createStackEvent({
-                    resourceIdPerCloudformation: "rootStackName",
-                    resourceIdPerTheServiceItsFrom:
-                      "arn:aws:cloudformation:us-east-1:000000000000:stack/rootStackName/00aa00a0-a00a-00aa-0a00-00a0a0a00000",
-                    resourceStatus: "REVIEW_IN_PROGRESS",
-                    resourceType: "AWS::CloudFormation::Stack",
-                    timestamp: new Date("2022-04-11T00:00:00.000Z"),
-                  }),
-                ],
-              });
-            default:
-              throw new Error(`${stackName} not found in the mock's setup`);
-          }
-        },
-      },
-    },
-  };
-
-  //ACT
-  const outputs = await transformStackEventDataIntoTracingData(inputs);
-
-  //ASSERT
-  const spanDataByConstructedId = new Map<string, ISpanData>();
-
-  const rootConstructedId =
-    "rootStackName-arn:aws:cloudformation:us-east-1:000000000000:stack/rootStackName/00aa00a0-a00a-00aa-0a00-00a0a0a00000-AWS::CloudFormation::Stack";
-
   spanDataByConstructedId.set(
-    "rootStackName-arn:aws:cloudformation:us-east-1:000000000000:stack/rootStackName/00aa00a0-a00a-00aa-0a00-00a0a0a00000-AWS::CloudFormation::Stack",
-    {
-      childSpanIds: new Set<string>(["TheEcsCluster-TheClusterName-AWS::ECS::Cluster"]),
-      name: "rootStackName",
-      startInstant: new Date("2022-04-11T00:00:05.000Z"),
-      endInstant: new Date("2022-04-11T00:00:30.000Z"),
-    },
-  );
-
-  spanDataByConstructedId.set(
-    "TheEcsCluster-TheClusterName-AWS::ECS::Cluster",
+    "TheEcsService-TheEcsServiceName-AWS::ECS::Service",
     {
       childSpanIds: new Set<string>(),
-      name: "TheEcsCluster",
-      startInstant: new Date("2022-04-11T00:00:10.000Z"),
-      endInstant: new Date("2022-04-11T00:00:15.000Z"),
+      name: "TheEcsService",
+      startInstant: new Date("2022-04-11T00:00:17.000Z"),
+      endInstant: new Date("2022-04-11T00:00:18.000Z"),
     },
   );
 
